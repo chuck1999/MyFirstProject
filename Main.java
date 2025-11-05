5735aab59a54520e3c87892db13456cd2efc53a2
chuck1999 <ccdeyarmon@gmail.com>
2025-11-05 14:45:33 -0800

    Add files via upload
    This is the file from our previous Lab Assignment 18 about try and catch

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO A1: Create a File object for "grades.txt"
+        File myFile = new File("grades.txt");
+        // TODO A2: Use try–catch to handle FileNotFoundException
+        try {
+            // TODO B1: Create a Scanner to read the file
+            Scanner input = new Scanner(myFile);
+            // TODO B2: Print "Student Grades:"
+            System.out.println("Student Grades:");
+            // TODO B3: Read and print each line from the file
+            while (input.hasNextLine()) {
+                System.out.println(input.nextLine());
+            }
+            input.close();
+        } catch (Exception e) {
+            // TODO C1: Print "File not found!" if grades.txt is missing
+            System.out.println("File not found!");
+        }
+    }
+}
